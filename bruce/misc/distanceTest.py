b4af2fbdd1e6a90f928eaba17259166500f7e9b0
Yifei Li 李逸飞 <liyifei723@gmail.com>
2023-04-07 20:40:56 -0500

    rename

--- bruce/misc/distanceTest.py
+++
@@ -1,7 +0,0 @@
-from gpiozero import DistanceSensor
-from time import sleep
-
-sensor = DistanceSensor(echo=25, trigger=9)
-while True:
-    print('Distance: ', sensor.distance * 100)
-    sleep(.5)
