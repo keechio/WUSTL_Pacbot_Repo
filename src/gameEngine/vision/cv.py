29b828f2b3406e6c94e4a3cae65f41c4f3239ba4
xucheng-wang <65243164+xucheng-wang@users.noreply.github.com>
2023-04-15 14:21:42 -0500

    Update cv.py

--- src/gameEngine/vision/cv.py
+++ src/gameEngine/vision/cv.py
@@ -13,8 +13,8 @@ class MovementProcessor(rm.ProtoModule):
     def __init__(self, addr, port, cam_id, y_off, height, width, show_windows, flip_v=False, flip_h=False):
         super().__init__(addr, port, message_buffers, MsgType, FREQUENCY)
         self.cap = cv2.VideoCapture(cam_id)
-        self.cap.set(3,640)
-        self.cap.set(4,360)
+        self.cap.set(3,960)
+        self.cap.set(4,540)
         self.y_off = y_off
         self.height = height
         self.width = width
@@ -56,8 +56,15 @@ class MovementProcessor(rm.ProtoModule):
         mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
         res = cv2.bitwise_and(warped, warped, mask=mask)
         res = cv2.cvtColor(res,cv2.COLOR_HSV2BGR)
+
         imgray = cv2.cvtColor(res,cv2.COLOR_BGR2GRAY)
         contours, _ = cv2.findContours(imgray,cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+        cv2.imshow('mask', mask)  # Shows the mask with the blue regions detected
+        cv2.imshow('res', res)    # Shows the extracted blue regions from the warped image
+        cv2.drawContours(warped, contours, -1, (0, 255, 0), 3)  # Draws the contours on the warped image
+        cv2.imshow('contours', warped)  # Shows the warped image with the drawn contours
+
         
 
         sector_h = imgray.shape[0]/self.height
@@ -65,13 +72,17 @@ class MovementProcessor(rm.ProtoModule):
 
         if len(contours) != 0:
             c = max(contours, key = cv2.contourArea)
-            if cv2.contourArea(c) > 100:
+            print(cv2.contourArea(c))
+
+            #modify this to allow more tolerance
+            if cv2.contourArea(c) > 80:
                 skip = False
                 x, y, w, h = cv2.boundingRect(c)
                 x_c = x + 0.5*sector_w
                 y_c = (imgray.shape[0] - y) - 1.5*sector_h
-                b_y = int(self.y_off + round(y_c/sector_h))
+                b_y = self.y_off + y_c/sector_h
                 b_x = int(round(x_c/sector_w))
+                b_y = int(round((b_y-15)*2+1))
                 if b_y >= self.y_off + self.height - 1 and self.y_off > 1:
                     b_y = self.y_off + self.height - 2
                 elif b_y < 1:
@@ -92,15 +103,16 @@ class MovementProcessor(rm.ProtoModule):
                     b_y = 29
 
                 if grid[b_x][b_y] == I:
+                    print('skipped')
                     skip = True
-                print((b_x, b_y))
+                print(b_x, b_y)
                 if not skip:
                     buf = PacmanState.AgentState()
                     buf.x = b_x
                     buf.y = b_y
                     self.client.write(buf.SerializeToString(), MsgType.PACMAN_LOCATION)
 
-        if self.show_windows:
+        if True:
             cv2.imshow('warp',warped) #Warped Image based on the corners detected.
 
     def kill(self):
