0d5d664d5e36b5c666fbf42df6f06444df09bdc9
xucheng-wang <65243164+xucheng-wang@users.noreply.github.com>
2023-04-15 14:18:16 -0500

    Update variables.py

--- src/gameEngine/vision/variables.py
+++ src/gameEngine/vision/variables.py
@@ -8,8 +8,14 @@ n = 5
 
 # Threshold values for yellow dots
 # Hue, saturation, value
-lower_yellow = np.array([20,50,112])
-upper_yellow = np.array([38,190,255])
+lower_yellow = np.array([20,200,100])
+upper_yellow = np.array([50,255,255])
 
-lower_green = np.array([40,44,100])
-upper_green = np.array([70,150,255])
+lower_green = np.array([50,100,100])
+upper_green = np.array([70,255,255])
+
+lower_red = np.array([0,100,100])
+upper_red = np.array([9,255,255])
+
+lower_blue = np.array([100,170,100])
+upper_blue = np.array([130,255,255])
